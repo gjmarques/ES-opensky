ab260749732c1b4ae9d9b4e15216ef8742a0360f
gjmarques <gjmarques@ua.pt>
2020-03-08 23:15:21 +0000

    implemented query persistency

--- complete/src/main/java/com/example/consumingrest/Arrival.java
+++ complete/src/main/java/com/example/consumingrest/Arrival.java
@@ -3,9 +3,21 @@ package com.example.consumingrest;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="arrival")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Arrival {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy = "uuid")
 	
 	private String icao24;
 	private int firstSeen;
--- complete/src/main/java/com/example/consumingrest/GreetingController.java
+++ complete/src/main/java/com/example/consumingrest/GreetingController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -22,33 +23,40 @@ import org.springframework.web.client.RestTemplate;
 @Component
 @RestController
 public class GreetingController {
-	
+	@Autowired
+	private UserRepository userRepository;
+
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-	
+
 	private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
 
 	@Scheduled(fixedRate = 5000)
 	public void getArrivalInfo() {
 		log.info("Fetching updated info....");
 		List<Arrival> arrivals = getArrivals();
+		log.info("UPDATE :" + arrivals);
 	}
 
 	static RestTemplate restTemplate = new RestTemplate();
-	
+
 	public List<Arrival> getArrivals(){
 		ResponseEntity<List<Arrival>> responses = restTemplate.exchange(
 				"https://USERNAME:PASSWORD@opensky-network.org/api/flights/departure?airport=EDDF&begin=1517230000&end=1517230800",
 				HttpMethod.GET,
-				  null,
-				  new ParameterizedTypeReference<List<Arrival>>(){});
+				null,
+				new ParameterizedTypeReference<List<Arrival>>(){});
 		List<Arrival> arrivals = responses.getBody();
+		for(int i=0;i<arrivals.size();i++) {
+		userRepository.save(arrivals.get(i));
+		}
 		return arrivals;
-		
+
 	}
+
 	@RequestMapping(value="/state",  method = RequestMethod.GET)
 	public String Teams () {
 		List<Arrival> arrival = getArrivals();
-		System.out.print("RESPONSES :" + getArrivals());
+		
 		String html = "<table>\n" + 
 				"  <tr>\n" + 
 				"    <th>PlaneID </th>\n" + 
@@ -56,17 +64,18 @@ public class GreetingController {
 				"    <th>Departure Airport </th>\n" + 
 				"    <th>Arrival Airport </th>\n" +
 				"  </tr>\n" ;
-				for(int i=0;i<arrival.size();i++) {
-					html += "<tr>\n" +
-								"<td>" + arrival.get(i).getIcao24() + "</td>" +
-								"<td>" + arrival.get(i).getCallsign() + "</td>" +
-								"<td>" + arrival.get(i).getEstDepartureAirport() + "</td>" +
-								"<td>" + arrival.get(i).getEstArrivalAirport() + "</td>" +
-							"</tr>\n";	
-				}
-				html += "</table>";
-				
-		
+		for(int i=0;i<arrival.size();i++) {
+
+			html += "<tr>\n" +
+					"<td>" + arrival.get(i).getIcao24() + "</td>" +
+					"<td>" + arrival.get(i).getCallsign() + "</td>" +
+					"<td>" + arrival.get(i).getEstDepartureAirport() + "</td>" +
+					"<td>" + arrival.get(i).getEstArrivalAirport() + "</td>" +
+					"</tr>\n";	
+		}
+		html += "</table>";
+
+
 		return html;	
 	}
 
